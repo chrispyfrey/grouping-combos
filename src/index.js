d724b5e7b1d7ef4b5f3a64f81971f9c5fe00432a
Chris Frey <christopher.r.frey1@gmail.com>
2022-10-11 21:48:14 -0600

    fix incorrect combination logic

--- src/index.js
+++ src/index.js
@@ -16,28 +16,44 @@ function ComboApp() {
         parsedInput = parsedInput.trim();
         
         if (parsedInput.search(' ') > 0) {
-            const parsedInputList = parsedInput.split(' ');
-            const comboList = [];
+            const splitDimArr = parsedInput.split(' ');
+            const dimDictArr = [];
+
+            for (let i = 0; i < splitDimArr.length; ++i) {
+                dimDictArr.push({'dim': splitDimArr[i], 'ndx': i+1});
+            }
+            
+            const outArr = [[{'dim': '', 'ndx': 0}], dimDictArr];
             let tmpArr = null;
+            let prvDimDictArr = null;
 
-            for (let n = 1; n < parsedInputList.length; ++n) {
-                for (let i = 0; i+n < parsedInputList.length; ++i) {
-                    for (let j = i+n; j < parsedInputList.length; ++j) {
-                        tmpArr = parsedInputList.slice(i, i+n);
-                        tmpArr.push(parsedInputList[j]);
-                        tmpArr[0] = '(' + tmpArr[0];
+            for (let i = 0; i < splitDimArr.length-1; ++i) {
+                tmpArr = [];
+                prvDimDictArr = outArr[outArr.length-1];
 
-                        if (n+1 < parsedInputList.length) 
-                            tmpArr[tmpArr.length-1] = tmpArr[tmpArr.length-1] + '),\n';
-                        else 
-                            tmpArr[tmpArr.length-1] = tmpArr[tmpArr.length-1] + ')';
-                        
-                        comboList.push(tmpArr.join(', '));
+                for (let j = 0; j < prvDimDictArr.length; ++j) {
+                    for (let k = prvDimDictArr[j]['ndx']; k < splitDimArr.length; ++k) {
+                        tmpArr.push({'dim': prvDimDictArr[j]['dim'] + ', ' + splitDimArr[k], 'ndx': k+1});
+                    }
+                }
+
+                outArr.push(tmpArr);
+            }
+            
+            const outStrArr = [];
+
+            for (let i = 0; i < outArr.length; ++i) {
+                for (let j = 0; j < outArr[i].length; ++j) {
+                    if (i+1 < outArr.length || j+1 < outArr[i].length) {
+                        outStrArr.push('(' + outArr[i][j]['dim'] + '),\n');
+                    }
+                    else {
+                        outStrArr.push('(' + outArr[i][j]['dim'] + ')');
                     }
                 }
             }
-            
-            setOutput(comboList.join(''));
+
+            setOutput(outStrArr.join(''));
         }
         else {
             setOutput('Error: Input must contain more than one dimension and be delimited by spaces, newlines, commas, or some combination.');
